abb84451924d161d8ec6c1debf6757d367a6da32
LLPeterX <LPeter@mail.ru>
2020-08-25 10:26:19 +0300

    show modal after timeout

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import './slider'
 import modals from './modules/modals'
+import formProcessor from './modules/formProcessor'
 
 window.addEventListener('DOMContentLoaded', () => {
   modals();
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,15 +1,15 @@
 const modals = () => {
   // bindModal() отвечает за привязку окна к триггеру
-  function bindModal(triggerSelector, modalSelector, closeSelector=null) {
-    const openButtons = document.querySelectorAll(triggerSelector),
+  function bindModal(triggerSelector, modalSelector, closeSelector = null) {
+    const openButtons = document.querySelectorAll(triggerSelector), // кнопок открытия может несколько, а внутри всего 2 случая
       modalWindow = document.querySelector(modalSelector),
-      closeButtons = document.querySelectorAll(closeSelector);
-    
+      closeBtn = document.querySelector(closeSelector);
+
     const closeWindow = (el) => {
       el.style.display = 'none';
       document.body.style.overflow = '';
     }
-      // клик на элемент открытия окна
+    // клик на элемент открытия окна
     openButtons.forEach((openBtn) => {
       openBtn.addEventListener('click', (e) => {
         if (e.target) {
@@ -21,37 +21,30 @@ const modals = () => {
         }
       });
     });
-    closeButtons && closeButtons.forEach(closeBtn => {
-      // клик на элемент закрытия окна 
-      closeBtn.addEventListener('click', () => {
-        closeWindow(modalWindow);
-//        modalWindow.style.display = 'none';
-//        document.body.style.overflow = '';
-        //document.body.classList.remove('modal-open');
-      });
+    // клик на элемент закрытия окна 
+    closeBtn.addEventListener('click', () => {
+      closeWindow(modalWindow);
     });
 
     // клик на подложку
     modalWindow.addEventListener('click', (e) => {
       if (e.target === modalWindow) {
-        //modalWindow.style.display = 'none';
-        //document.body.style.overflow = '';
         closeWindow(modalWindow);
-        //document.body.classList.remove('modal-open');
       }
     })
-
   }
 
-  // test
-  // const openBtn = document.querySelector('.popup_engineer_btn'),
-  //   modalWindow = document.querySelector('.popup_engineer'),
-  //   closeBtn = document.querySelector('.popup_engineer .popup_close');
-  // bindModal(openBtn, modalWindow, closeBtn);
+  // показать окно через 60 сек.
+  const showModalAfterTimeout = (selector, timeout) => {
+    setTimeout(()=> {
+      document.querySelector(selector).style.display='block';
+      document.querySelector(selector).overflow='hidden';
+    },timeout);
+  }
 
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
-  bindModal('.phone_link','.popup','.popup_dialog .popup_close');
-//  bindModal('.contact_us','.popup','.popup_dialog .popup_close');
+  bindModal('.phone_link', '.popup', '.popup .popup_close');
+  showModalAfterTimeout('.popup',60000);
 }
 
 export default modals;
